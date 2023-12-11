35b5fbf58ed9031569fc121cad7ec8e3131d7965
Vladimir N. Silyaev <vsilyaev@gmail.com>
2023-12-11 01:55:49 +0000

    Support architecture flavors

--- .devcontainer/features/musl-cross/install.sh
+++ .devcontainer/features/musl-cross/install.sh
@@ -15,10 +15,15 @@ host='static'
 
 for arch in ${ARCHITECTURES}; do
 # https://github.com/TheCBaH/musl-cross-make.builder/releases/download/v0.0.2/musl-cross-aarch64-linux-musl-gcc9.4.0-static.tar.xz
-    fname="musl-cross-$arch-linux-musl-gcc$gcc-$host.tar.xz"
+    arch_base="${arch%.*}"
+    arch_flavor=''
+    if [ "$arch_base" != "$arch" ]; then
+        arch_flavor="${arch#*.}"
+    fi
+    fname="musl-cross-$arch_base-linux-musl$arch_flavor-gcc$gcc-$host.tar.xz"
     url="$base_url/$ver/$fname"
     fname="/tmp/$fname"
-    curl --location --show-error -o $fname $url
+    curl --fail --location --show-error -o $fname $url
     xz -d $fname
     fname="${fname%.xz}"
     tar -xf $fname -C ${1:-/usr/local}
